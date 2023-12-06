65759344697c3721fe7be49d38f1c9d6a8cf7d8e
Ethan Ness <ness0321@d.umn.edu>
2023-12-06 17:41:42 -0600

    Working CSV Converter
    Working CSV Converter which converts the CSV file into a list and there is a page which views all the information.

--- lib/Models/model.dart
+++ lib/Models/model.dart
@@ -1,6 +1,5 @@
 // Selected Page Data
 
-import 'dart:js';
 import 'package:csv/csv.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -14,17 +13,6 @@ class SelectedPage {
 
   getSelectedIndex()  {
     return selectedIndex;
-  }
-
-}
-
-class CSVConverter{
-
-  Future<List<List<dynamic>>> processCSV() async {
-    var result = await DefaultAssetBundle.of(context as BuildContext).loadString(
-      "assets/Amazon_Books_Data.csv",
-    );
-    return const CsvToListConverter().convert(result, eol: "\n");
   }
 
 }
--- lib/views/CSV_View.dart
+++ lib/views/CSV_View.dart
@@ -1,11 +1,25 @@
-import 'package:flutter/cupertino.dart';
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
-import 'package:group_d_final/Models/model.dart';
+//import 'package:group_d_final/Models/model.dart';
 
-class CSV_View extends StatelessWidget{
-  CSV_View({super.key});
+class CSV_View extends StatefulWidget {
+  const CSV_View({super.key});
+
+  @override
+  _CSV_ViewState createState() => _CSV_ViewState();
+}
+
+
+class _CSV_ViewState extends State<CSV_View>{
 
   List<List<dynamic>>? csvFile;
+
+  Future<List<List<dynamic>>> processCSV() async {
+    var result = await DefaultAssetBundle.of(context).loadString(
+      "assets/Amazon_Books_Data.csv",
+      );
+    return const CsvToListConverter().convert(result, eol: "\n");
+}
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +27,47 @@ class CSV_View extends StatelessWidget{
       appBar: AppBar(
         title: const Text("CSV to text"),
       ),
-
-    );
-  }
-
+        body: ListView(
+          children: [
+        SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: csvFile == null
+                  ? const CircularProgressIndicator()
+                  : DataTable(
+                columns: csvFile![0]
+                    .map(
+                      (item) => DataColumn(
+                    label: Text(
+                      item.toString(),
+                    ),
+                  ),
+                )
+                    .toList(),
+                rows: csvFile!
+                    .map(
+                      (csvrow) => DataRow(
+                    cells: csvrow
+                        .map(
+                          (csvItem) => DataCell(
+                        Text(
+                          csvItem.toString(),
+                        ),
+                      ),
+                    )
+                        .toList(),
+                  ),
+                )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            csvFile = await processCSV();
+            setState(() {});
+          },
+        ),
+      );
+    }
 }
