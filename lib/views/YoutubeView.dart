24e519f5f9ab7fec0a6ca3c6c50f54a1d8b2feb4
amach013 <amach013@d.umn.edu>
2023-12-11 20:41:50 -0600

    Added video

--- lib/views/YoutubeView.dart
+++ lib/views/YoutubeView.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:collection/collection.dart'; // Add this for groupBy extension
+import 'package:collection/collection.dart';
 
 class VideoInfo {
   final String topic;
