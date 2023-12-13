04c7a58dc6f24aa7402a9aaef46de677cfc2dd4e
Ethan Schroers <schr1590@umn.edu>
2023-12-13 00:19:58 -0600

    changed welcome page UI

--- lib/views/WelcomePage.dart
+++ lib/views/WelcomePage.dart
@@ -10,7 +10,111 @@ class welcomePage extends StatefulWidget {
 }
 
 class _welcomePageState extends State<welcomePage> {
+
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "assets/Images/welcomeBackground.jpg"
+                      ),
+                    fit: BoxFit.fill
+                  ),
+                  /*gradient: LinearGradient(
+                      colors: [
+                        Colors.lightBlueAccent,
+                        Color(0xFF01579B)
+                      ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight
+                  )*/
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height-750,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                ),
+                child: Center(
+                  child: Text(
+                    "CS Book Helper",
+                    style: GoogleFonts.aBeeZee(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(
+                    left: 5,
+                    right: 5
+                  ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height-550,
+                    ),
+                    SizedBox(
+                      height: 300,
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        "Learn, \nApply, \nCreate",
+                        style: GoogleFonts.aBeeZee(
+                            color: Colors.white,
+                            fontSize: 70,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => const loginPage()));
+                        },
+                        child: Container(
+                          height: 90,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Get Started",
+                              style: GoogleFonts.aBeeZee(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40,
+                                color: Colors.black
+                              ),
+                            ),
+                          )
+                        )
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+  /*@override
   Widget build(BuildContext context) {
     return Container(
       height: double.infinity,
@@ -64,7 +168,7 @@ class _welcomePageState extends State<welcomePage> {
         ],
       ),
     );
-  }
+  }*/
 
 
 }
